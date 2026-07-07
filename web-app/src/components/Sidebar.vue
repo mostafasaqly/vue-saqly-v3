89dae3b968a6b28bfa5815ef1d213130a11b0d7d
Mostafa Saqly <mostafasaqly1@gmail.com>
2026-07-07 14:26:26 +0300

    update sidebar by add next app icon for web app using next

--- web-app/src/components/Sidebar.vue
+++ web-app/src/components/Sidebar.vue
@@ -79,6 +79,35 @@ function select(id) {
             <path d="M125 52.1L66.8 182.6h21.7l11.7-29.2h49.4l11.7 29.2H183L125 52.1zm17 83.3h-34l17-40.9 17 40.9z" fill="#fff"/>
           </svg>
         </a>
+        <a
+          href="https://mostafasaqly.github.io/next-saqly-v16/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="sidebar__course-link sidebar__course-link--next"
+          :title="isAr ? 'كورس Next.js 16' : 'Next.js 16 Course'"
+          :aria-label="isAr ? 'انتقل إلى كورس Next.js' : 'Go to Next.js Course'"
+        >
+          <svg class="sidebar__next-logo" viewBox="0 0 180 180" xmlns="http://www.w3.org/2000/svg">
+            <mask id="next-mask" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="180" height="180">
+              <circle cx="90" cy="90" r="90" fill="black"/>
+            </mask>
+            <g mask="url(#next-mask)">
+              <circle cx="90" cy="90" r="90" fill="black"/>
+              <path d="M149.508 157.52L69.142 54H54v71.97h12.114V69.384l73.885 95.461a90.304 90.304 0 0 0 9.509-7.325z" fill="url(#next-grad1)"/>
+              <rect x="115" y="54" width="12" height="72" fill="url(#next-grad2)"/>
+            </g>
+            <defs>
+              <linearGradient id="next-grad1" x1="109" y1="116.5" x2="144.5" y2="160.5" gradientUnits="userSpaceOnUse">
+                <stop stop-color="white"/>
+                <stop offset="1" stop-color="white" stop-opacity="0"/>
+              </linearGradient>
+              <linearGradient id="next-grad2" x1="121" y1="54" x2="120.799" y2="106.875" gradientUnits="userSpaceOnUse">
+                <stop stop-color="white"/>
+                <stop offset="1" stop-color="white" stop-opacity="0"/>
+              </linearGradient>
+            </defs>
+          </svg>
+        </a>
       </div>
     </div>
 
